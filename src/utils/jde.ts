00a364728b98f5752fbe64c0ec85736fdae3e06c
Orlando Mendoza <ommv.17@gmail.com>
2023-06-06 11:08:17 -0400

    Cambios en la función de fechas de JDE

--- src/utils/jde.ts
+++ src/utils/jde.ts
@@ -1,18 +1,26 @@
 import { format, addDays } from "date-fns"
 
 const getJDEdwardsJulianDate = (date: string) => {
-  
-  const getYear = (date: string) => {
-    const yearFormat = parseInt(date.split("-")[0])
-    
-    const year = parseInt(format(new Date(yearFormat, 0, 1), "yyyy"))
-    const shortYear = format(new Date(yearFormat, 0, 1), "yy")
-    
-    const twenty_one_century = 2000
 
-    const julianYear = (year >= twenty_one_century) ? `1${shortYear}` : `0${shortYear}`
-    console.log("year", julianYear);
+  const getYear = (date: string) => {
+    // string = "2023-01-02"
+    const yearFormat = parseInt(date.split("-")[0])
+
+    const JANUARY_MONTH = 0
+    const DAY = 1
+
+    const dateInstance = new Date(yearFormat, JANUARY_MONTH, DAY)
     
+    const year = parseInt(format(dateInstance, "yyyy"))
+    const shortYear = format(dateInstance, "yy")
+
+    const TWENTY_ONE_CENTURY = 2000
+    const ABOVE = "1"
+    const BELLOW = "0"
+
+    // const julianYear = (year >= twenty_one_century) ? `1${shortYear}` : `0${shortYear}`
+
+    const julianYear = `${(year >= TWENTY_ONE_CENTURY) ? ABOVE : BELLOW}${shortYear}`
     return julianYear;
   }
 
@@ -20,11 +28,9 @@ const getJDEdwardsJulianDate = (date: string) => {
 
     const ADDITIONAL_DAY = 1
     const ONE_DAY_MORE_DATE = addDays(new Date(date), ADDITIONAL_DAY);
-    
-    const day = format(ONE_DAY_MORE_DATE, "DDD")
 
-    console.log("day", day);
-    return day;
+    const dayOfTheYear = format(ONE_DAY_MORE_DATE, "DDD")
+    return dayOfTheYear;
   }
 
   const year = getYear(date)
